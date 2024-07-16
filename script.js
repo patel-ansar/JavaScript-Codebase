122b8e9bbff58fe787b8536e9bf91ed54774ecaf
patel-ansar <er.mohdansar888@gmail.com>
2024-07-16 11:26:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const product = {
+    title : "Parker Jotter Standard CT Ball Pen (Black)",
+    rating : 4,
+    isDeal : true,
+    price : 270,
+    offer : 5
+};
+product["offer"] = product["offer"] + 5;
+
+const profile = {
+    userName : "@aaftaab",
+    followers : 50,
+    following : 125,
+    isFollow : true
+};
